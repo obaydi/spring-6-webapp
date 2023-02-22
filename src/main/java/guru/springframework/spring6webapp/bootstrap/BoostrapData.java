fea7c79a85e5d29598fb3bd9d0a112c5173d5e8f
obaydi <hasan@obaydi.co.uk>
2023-02-22 14:07:03 +0000

    Added Publisher

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BoostrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BoostrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BoostrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BoostrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BoostrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,8 +32,17 @@ public class BoostrapData implements CommandLineRunner {
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("Fisher");
+        publisher.setAddress("10 King Street");
+        publisher.setCity("London");
+        publisher.setState("UK");
+        publisher.setZip("90210");
+
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
+        Publisher savedPublisher = publisherRepository.save(publisher);
+        ddd.setPublisher(savedPublisher);
 
         Author rod = new Author();
         rod.setFirstName("Rod");
@@ -44,9 +57,18 @@ public class BoostrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
+        noEJB.setPublisher(savedPublisher);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
